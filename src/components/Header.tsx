64188d39c934a527f21fef13e07e4253535f025b
Tiavina <tiavinaramilison22@gmail.com>
2025-04-14 08:50:29 +0300

    [FIX]: fix name app in the header components.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export const Header: React.FC = () => {
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
             <Link to="/" className="text-xl font-bold text-gray-900 dark:text-white">
-              GitHub Stats
+              GitDash
             </Link>
           </div>
 
